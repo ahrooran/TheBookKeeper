4aaa0ebf92732298397b636d2c13e834c9f8cf0b
ahrooran <ahrooran@hotmail.co.uk>
2019-07-02 00:01:33 +0100

    Add files via upload

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -3,7 +3,6 @@ import DeleteBtn from "../components/DeleteBtn";
 import ViewBtn from '../components/ViewBtn'
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-//import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 
