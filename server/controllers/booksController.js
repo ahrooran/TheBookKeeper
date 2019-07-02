dd74ecd280a10bce06c14754000ab16b380cd33c
ahrooran <ahrooran@hotmail.co.uk>
2019-07-02 14:08:20 +0100

    Add files via upload

---
+++ server/controllers/booksController.js
@@ -0,0 +1,31 @@
+const db = require('../models');
+
+module.exports = {
+  create: (req, res) => {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+  },
+  update: (req, res) => {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel)) 
+  },
+  remove: (req, res) => {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+  },
+  findById: (req, res) => {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+  },
+  findAll: (req, res) => {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+  },
+};
