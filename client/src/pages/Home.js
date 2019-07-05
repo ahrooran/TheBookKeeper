ccb5c86be3316355142a7b06eae0d723bb203786
ahrooran <ahrooran@hotmail.co.uk>
2019-07-05 10:10:50 +0100

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,55 +1,10 @@
 import React, { Component } from 'react';
 import { Col, Row, Container } from '../components/Grid';
 import Jumbotron from '../components/Jumbotron';
-import API from "../utils/API";
 import './style.css';
 import { List, ListItem } from '../components/List';
 
 class Detail extends Component {
-  state = {
-    books: {},
-    title: ''
-  };
-  
-  componentDidMount() {
-    this.setState({ books: {}, title: '' });
-  }
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
-
-  saveBook = bookID => {
-    const book = this.state.books.find(book => book.id === bookID);
-    API.saveBook({
-      title: book.volumeInfo.title,
-      author: book.volumeInfo.authors[0],
-      description: book.volumeInfo.description,
-      image: book.volumeInfo.imageLinks.thumbnail,
-      link: book.volumeInfo.previewLink
-    }).then(() => {
-      this.setState({
-        books: this.state.books.filter(book => book.id !== bookID)
-      });
-    }).catch(err => console.log(err));
-  }
-
-  viewBook = url => {
-    window.location = url;
-  }
-
-  showBooks = data => {
-    this.setState({ books: data.data.items });
-  }
-
-  searchBook = event => {
-    event.preventDefault();
-
-    API.getTitles(this.state.title)
-    .then(results => this.showBooks(results))
-    .catch(err => console.log(err));
-  }
 
   render() {
     return (
